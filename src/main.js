785be8f69eed4b3a8ea2d633ba9414f9182dad9a
myruldeen <funstudy10@gmail.com>
2024-11-17 18:58:42 +0800

    fix git issue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import './assets/main.css'
 import './style.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
-app.use(createPinia())
+
+app.use(router)
+
 app.mount('#app')
